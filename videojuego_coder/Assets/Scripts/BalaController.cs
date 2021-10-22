5457d4fc5c1334a2120612dde673009393685ecf
María Belén Trigueros <bel.trigueros@gmail.com>
2021-10-21 23:49:49 -0300

    Desafio 4
    Disparos temporizados

--- videojuego_coder/Assets/Scripts/BalaController.cs
+++ videojuego_coder/Assets/Scripts/BalaController.cs
@@ -7,6 +7,8 @@ public class BalaController : MonoBehaviour
     public float speedBala = 0.5f; //velocidad de la bala
     public Vector3 dir = new Vector3(1, 0, 0); //direccion de la bala
     public int balaDamage = 1; //daño que provoca la bala
+    public float timeBala = 3f; //tiempo en que la bala estara en la escena antes de ser eliminada
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,18 @@ public class BalaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        BalaMovement(new Vector3(speedBala, 0, 0)); //inicio el metodo de movimiento en el update
+
+        BalaNewScale(2); //llamo al metodo para multiplicar la escala de la bala
+
+        timeBala -= Time.deltaTime; //al tiempo de la bala le voy restando el tiempo del juego, asi logro determinar si la bala sera destruida o no.
+        if (timeBala > 0) //si el tiempo de la bala es mayor a 0, esta se movera
+        {
+            BalaMovement(new Vector3(speedBala, 0, 0));
+        }
+        else // si el tiempo llega a 0 sera destruida
+        {
+            Destroy(gameObject);
+        }
     }
 
     //metodo para el movimiento de la bala
@@ -25,4 +38,15 @@ public class BalaController : MonoBehaviour
         dir = nuevaDir;
         return (transform.position += dir) * Time.deltaTime;
     }
+
+    //metodo para cambiar la escala de la bala
+    public void BalaNewScale(int multiplo) //paso por parametro el nro por el cual quiero multiplicar la escala
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space)) //si el usuario presiona la barra espaciadora, la escala de la bala se multiplica por el numero establecido en el parametro
+        {
+            transform.localScale += transform.localScale * multiplo;
+        }
+    }
+    
 }
--- videojuego_coder/Assets/Scripts/GeneratorController.cs
+++ videojuego_coder/Assets/Scripts/GeneratorController.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class GeneratorController : MonoBehaviour
 {
-    public GameObject[] balasPrefab; //array con los prefab de las balas
+    public GameObject balasPrefab; //array con los prefab de las balas
     public float disparoSpeedInit = 1f; //velocidad inicial del disparador
     public float disparoSpeedRepetition = 0.2f; //tiempo que pasa entre una bala y la siguiente
+    public float cooldown = 0.5f; //tiempo de descanso entre cada disparo
+    public bool beenShot = false; //variable booleana que establece si la bala ha sido disparada o no
+    float timePassed = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Disparo", disparoSpeedInit, disparoSpeedRepetition); //metodo invokerepeating, declaro el metodo como string, y luego su velocidad inicial y la velocidad de repeticion
+       // InvokeRepeating("Disparo", disparoSpeedInit, disparoSpeedRepetition); //metodo invokerepeating, declaro el metodo como string, y luego su velocidad inicial y la velocidad de repeticion
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DisparoControlado(); //llamo al metodo en el update
     }
 
-    //metodo para disparar las balas
-    void Disparo() 
+    //metodo para disparar las balas de manera automatica
+    //void Disparo() 
+    //{
+      //  int balasIndex = Random.Range(0, balasPrefab.Length); //declaro un rango entre 0 y el largo del array de balas para inicializarlas de manera aleatoria
+        //Instantiate(balasPrefab[balasIndex], transform); //metodo instantiate para inicializar las balas a partir del array y el transform del padre. 
+    //}
+
+    //metodo para disparar las balas mediante un input
+    public void DisparoControlado() 
     {
-        int balasIndex = Random.Range(0, balasPrefab.Length); //declaro un rango entre 0 y el largo del array de balas para inicializarlas de manera aleatoria
-        Instantiate(balasPrefab[balasIndex], transform); //metodo instantiate para inicializar las balas a partir del array y el transform del padre. 
+        if (Input.GetKeyDown(KeyCode.Z) && !beenShot) //si el usuario apreta la tecla Z y una bala NO ha sido disparada, se instanciara una bala
+        {
+            beenShot = true; //se establece que ha sido disparado la bala como true
+            Instantiate(balasPrefab, transform);
+        }
+        if (beenShot) //si beenShot es true, se empieza a contar el tiempo pasado
+        {
+            timePassed += Time.deltaTime;
+        }
+
+        if (timePassed > cooldown) // si el tiempo pasado es mayor al establecido en el cooldown, se reinicia el contador, y la variable beenShot vuelve a ser falsa. De esta manera, se puede volver a instanciar una nueva bala.
+        {
+            beenShot = false;
+            timePassed = 0;
+        }
+
     }
 }
+
+
